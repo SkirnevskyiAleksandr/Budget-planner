d9fa5ffbac3d2a3d160e4c6436550cdff4f71bc3
Aleksandr <sanya-sha2016@ukr.net>
2021-01-05 23:45:22 +0200

    2 lesson

--- javaScript.js
+++ javaScript.js
@@ -1,17 +1,44 @@
-var money = prompt( "Ваш бюджет на месяц?");
-var time = prompt("Введите дату в формате YYYY-MM-DD");
-var appData = {
-    budget:money,
+let money = +prompt("Ваш бюджет на месяц?", ''),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
     timeData: time,
-    expenses : { },
-    optionalExpenses : {},
-    income : [],
     savings: false
 };
-var whatBying = prompt("Введите обязательную статью расходов в этом месяце");
-var hawMatch = prompt("Во сколько обойдется?");
 
-appData.expenses.whatBying = whatBying;
-appData.expenses.hawMatch = hawMatch;
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", '');
+    let b = prompt("Во сколько обойдется?", '');
+    
+    if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != ""
+     && b != "" && a.length < 50)
+    {    
+        // console.log(done);
+        appData.expenses[a] = b;
+    }
 
-alert(appData.budget/30);
+    else{
+        alert("rone imput!");
+        i--;
+    };
+
+};
+
+appData.moneyPerDay=appData.budget/30;
+alert(appData.moneyPerDay)
+alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+
+
+if (appData.moneyPerDay < 100) {
+    console.log ("Это минимальный уровень достатка!");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log ("Это средний уровень достатка!");
+} else if (appData.moneyPerDay > 2000) {
+    console.log ("Это высокий уровень достатка!");
+} else {
+    console.log ("Произошла ошибка");
+}
